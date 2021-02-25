260c4827fd9c9d5d310eb5a8718020b6baeb75d6
esroleo <esroleo@gmail.com>
2021-02-25 08:43:06 -0500

    inquirer test and appliation initialized as an object completed

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 // Extra note - MySql server version is Ver 8.0.23 Win64
 const mysql = require('mysql2');
 
+// import inquirer libraries
+const inquirer = require('inquirer');
+
 // npm library console.table to display tabulated format at console log
 const cTable = require('console.table');
  
@@ -16,9 +19,72 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    // initiqlize application and call our first prototype function 
+    new RunApplication().getInquirerOptions();
   });
 
+class RunApplication {
+  constructor () {
+      // *** Hold object arrays *** //
+      this.manager = [];
+      this.engineer = [];
+      this.intern = []
+      // *** Used for conditional chesk **//
+      this.employeeType = "";
+    }
 
+    getInquirerOptions() {
+
+          inquirer
+          .prompt([
+            {
+              type: 'list',
+              name: 'queryType',
+              message: "What would you like to do?",
+              choices: ['View all employees', 'Intern']
+            }
+          ])
+          // if answer is true go to next step
+          .then(answers => {
+            //console.log(answers);
+            const {queryType} = answers;
+            
+            if (queryType === "View all employees") {
+              connection.query(
+                `select e.first_name, e.last_name, r.title, dep.name AS department, 
+                r.salary,
+                IFNULL(CONCAT(m.first_name, ' ' , m.last_name), 'No Manager') AS 'manager'
+                from employee AS e
+                INNER JOIN role AS r ON e.role_id = r.id 
+                INNER JOIN department AS dep ON r.department_id = dep.id
+                LEFT JOIN employee AS m ON m.id = e.manager_id
+                ORDER BY e.first_name;`,
+                function(err, results, fields) {
+                  const table = cTable.getTable(results);
+                  console.table(table);
+                  //console.log(results);
+                  //console.log(results);
+                  //console.log(results); // results contains rows returned by server
+                  //console.log(fields); // fields contains extra meta data about results, if available
+                }
+              );
+            }
+            //this.employeeType = employeeType;
+            //this.getFurtherInformation();
+          });
+          
+          
+    
+
+
+    }
+
+
+}
+
+
+
+/*
     // View All Employees
 connection.query(
   `select e.first_name, e.last_name, r.title, dep.name AS department, 
@@ -74,3 +140,4 @@ connection.query(
     //   }
     // );
 
+*/
